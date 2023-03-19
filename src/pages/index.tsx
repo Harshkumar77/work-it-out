c4c0d3136b207b571b620cb7b5fb0c8ff43385d7
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2023-03-19 17:46:12 +0530

    Added Favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>WorkItOut</title>
         <meta name="description" content="Track workouts ,calories and progress" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <p>WorkItOut</p>
     </>
