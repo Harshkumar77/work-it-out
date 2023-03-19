45437b5c4822505c6892cc3582bf02df16d432b1
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2023-03-19 17:51:20 +0530

    updated description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <Head>
         <title>WorkItOut</title>
-        <meta name="description" content="Track workouts ,calories and progress" />
+        <meta name="description" content="Track workouts , calories , weight and progress" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
